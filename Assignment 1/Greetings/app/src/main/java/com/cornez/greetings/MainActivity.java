ea99e69261d0ac3073de8af6f80764e3dea01898
Brennen Nalley <46981701+Rebnnenx@users.noreply.github.com>
2020-01-27 16:32:08 -0500

    Fixed the issues

--- Assignment 1/Greetings/app/src/main/java/com/cornez/greetings/MainActivity.java
+++ Assignment 1/Greetings/app/src/main/java/com/cornez/greetings/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-       // getMenuInflater().inflate(R.menu.menu_main, menu);
+        // getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
