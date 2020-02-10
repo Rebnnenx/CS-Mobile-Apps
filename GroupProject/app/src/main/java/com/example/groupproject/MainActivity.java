6ad8074ceae683f300ce6b2b69cc8132485ba7ff
Brennen Nalley <46981701+Rebnnenx@users.noreply.github.com>
2020-02-10 15:49:43 -0500

    Created Snake Activity
    Created Snake Activity and also
    initalized snake engine (Not Functional at current time)

--- GroupProject/app/src/main/java/com/example/groupproject/MainActivity.java
+++ GroupProject/app/src/main/java/com/example/groupproject/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.groupproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSnakeClick(View v){
+        Intent myIntent = new Intent(getBaseContext(),   SnakeGame.class);
+        startActivity(myIntent);
     }
 }
---
+++ GroupProject/app/src/main/java/com/example/groupproject/SnakeGame.java
@@ -0,0 +1,48 @@
+package com.example.groupproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+
+
+
+
+public class SnakeGame extends AppCompatActivity {
+
+    //creates an instance of snake engine
+   // SnakeEngine snakeEngine;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snake_game);
+
+            // Get the dimensions of the screen
+            Display display = getWindowManager().getDefaultDisplay();
+
+            // Set pip size
+            Point size = new Point();
+            display.getSize(size);
+
+            // Create an instance of SnakeEngine class
+          //  snakeEngine = new SnakeEngine(this, size);
+
+            // Set snakeEngine to the view of Activity
+         //   setContentView(snakeEngine);
+    }
+
+//    @Override
+//    protected void resume() {
+//        super.resume();
+//        snakeEngine.Resume();
+//    }
+//
+//    @Override
+//    protected void pause() {
+//        super.pause();
+//        snakeEngine.pause();
+//    }
+
+}
