f6a960a474d54092a5781490be68ab537fc085e6
Brennen Nalley <46981701+Rebnnenx@users.noreply.github.com>
2020-02-24 14:54:29 -0500

    blah

---
+++ RentalCar/app/src/main/java/com/cornez/rentalcar/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cornez.rentalcar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
