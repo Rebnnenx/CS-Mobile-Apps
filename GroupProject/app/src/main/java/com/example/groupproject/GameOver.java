2429d4e69883e1b446e6d0fc9b20a1644b34d6d2
rlucas2011 <reneemlucas@gmail.com>
2020-03-16 17:45:59 -0400

    Buttons on the Game Over Screen
    Added buttons and code refrences to the game over screen

--- GroupProject/app/src/main/java/com/example/groupproject/GameOver.java
+++ GroupProject/app/src/main/java/com/example/groupproject/GameOver.java
@@ -3,15 +3,24 @@ package com.example.groupproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GameOver extends AppCompatActivity {
 
+    private Button home;
+    private Button playAgain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+
+        home = (Button) findViewById(R.id.btnHome);
+        playAgain = (Button) findViewById(R.id.btnPlayAgain);
+
+
     }
 
 }
